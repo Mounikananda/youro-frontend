4154d3b71ef30c1a8597895da2fc8fac6bef0b96
mandavaravi <mandavaravindra1998@gmail.com>
2023-10-11 22:34:31 -0400

    edit doctor status render and toggle

--- youro/src/views/Admin UI/Admin-HomePage.js
+++ youro/src/views/Admin UI/Admin-HomePage.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useMemo,  useCallback } from "react";
+import React, { useState, useRef, useMemo, useEffect, useCallback } from "react";
 import { MaterialReactTable } from 'material-react-table';
 import {
     Box,
@@ -13,13 +13,15 @@ import {
     MenuItem,
     Stack,
     TextField,
+    Container,
     Tooltip,
+    Switch
 } from '@mui/material';
+// import Container from '@mui/material/Container';
 
-import { Delete, Edit } from '@mui/icons-material';
+import { Delete } from '@mui/icons-material';
 import SideBar from '../Patient UI/SideBar';
 import "../../styles/Admin-ui/Admin-HomePage.css";
-
 const data = [
     {
         id: '1',
@@ -30,6 +32,7 @@ const data = [
         city: 'East Daphne',
         state: 'Kentucky',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '2',
@@ -40,6 +43,7 @@ const data = [
         city: 'Vancouver',
         state: 'British Columbia',
         country: 'Canada',
+        isApproved: true
     },
     {
         id: '3',
@@ -50,6 +54,7 @@ const data = [
         city: 'South Linda',
         state: 'West Virginia',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '4',
@@ -60,6 +65,7 @@ const data = [
         city: 'Lincoln',
         state: 'Nebraska',
         country: 'United States',
+        isApproved: false
     },
     {
         id: '5',
@@ -70,6 +76,7 @@ const data = [
         city: 'Charleston',
         state: 'South Carolina',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '1',
@@ -80,6 +87,7 @@ const data = [
         city: 'East Daphne',
         state: 'Kentucky',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '2',
@@ -90,6 +98,7 @@ const data = [
         city: 'Vancouver',
         state: 'British Columbia',
         country: 'Canada',
+        isApproved: false
     },
     {
         id: '3',
@@ -100,6 +109,7 @@ const data = [
         city: 'South Linda',
         state: 'West Virginia',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '4',
@@ -110,6 +120,7 @@ const data = [
         city: 'Lincoln',
         state: 'Nebraska',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '5',
@@ -120,6 +131,7 @@ const data = [
         city: 'Charleston',
         state: 'South Carolina',
         country: 'United States',
+        isApproved: false
     },
     {
         id: '1',
@@ -130,6 +142,7 @@ const data = [
         city: 'East Daphne',
         state: 'Kentucky',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '2',
@@ -140,6 +153,7 @@ const data = [
         city: 'Vancouver',
         state: 'British Columbia',
         country: 'Canada',
+        isApproved: true
     },
     {
         id: '3',
@@ -150,6 +164,7 @@ const data = [
         city: 'South Linda',
         state: 'West Virginia',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '4',
@@ -160,6 +175,7 @@ const data = [
         city: 'Lincoln',
         state: 'Nebraska',
         country: 'United States',
+        isApproved: false
     },
     {
         id: '5',
@@ -170,6 +186,7 @@ const data = [
         city: 'Charleston',
         state: 'South Carolina',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '1',
@@ -180,6 +197,7 @@ const data = [
         city: 'East Daphne',
         state: 'Kentucky',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '2',
@@ -190,6 +208,7 @@ const data = [
         city: 'Vancouver',
         state: 'British Columbia',
         country: 'Canada',
+        isApproved: true
     },
     {
         id: '3',
@@ -200,6 +219,7 @@ const data = [
         city: 'South Linda',
         state: 'West Virginia',
         country: 'United States',
+        isApproved: false
     },
     {
         id: '4',
@@ -210,6 +230,7 @@ const data = [
         city: 'Lincoln',
         state: 'Nebraska',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '5',
@@ -220,6 +241,7 @@ const data = [
         city: 'Charleston',
         state: 'South Carolina',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '1',
@@ -230,6 +252,7 @@ const data = [
         city: 'East Daphne',
         state: 'Kentucky',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '2',
@@ -240,6 +263,7 @@ const data = [
         city: 'Vancouver',
         state: 'British Columbia',
         country: 'Canada',
+        isApproved: true
     },
     {
         id: '3',
@@ -250,6 +274,7 @@ const data = [
         city: 'South Linda',
         state: 'West Virginia',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '4',
@@ -260,6 +285,7 @@ const data = [
         city: 'Lincoln',
         state: 'Nebraska',
         country: 'United States',
+        isApproved: true
     },
     {
         id: '5',
@@ -270,19 +296,73 @@ const data = [
         city: 'Charleston',
         state: 'South Carolina',
         country: 'United States',
+        isApproved: true
     },
 ];
 
 const AdminHomePage = () => {
-    const [render, canRender] = useState(false);
-    const url = `http://localhost:9092/youro/api/v1/symptomScore/`;
-    axios.get(url).then((res) => {
-        console.log("got symptom score :: " + JSON.stringify(res.data));
-        data = res.data;
-        canRender(true);
-    }).catch((err) => {
-        canRender(true);
-        console.error(err?.response?.data.errorMessage)
+    // const url = `http://localhost:9092/youro/api/v1/symptomScore/`;
+    // axios.get(url).then((res) => {
+    //     console.log("got symptom score :: " + JSON.stringify(res.data));
+    //     data = res.data;
+    // }).catch((err) => {
+    //     canRender(true);
+    //     console.error(err?.response?.data.errorMessage)
+    // });
+
+
+    /**
+     * 
+     * @param {JSON} row - table cell data 
+     * @param {boolean} isChange - isChangeEvent
+     * @returns boolean
+     */
+
+    const handleApproveRenderAndChange = (row = { emptyRow: true }, isChange = false) => {
+        let temp = tableData;
+        if (isChange) {
+            console.clear();
+            console.log('row => ' + JSON.stringify(row.row.original));
+            for (let i = 0; i < temp.length; i++) {
+                if (row.row.original.id == temp[i].id) {
+                        console.log('i ==>  ' + i + " :: " + temp[i].id + " -> " + temp[i].isApproved);
+                        temp[i].isApproved = !temp[i].isApproved;
+                        setTableData(temp);
+                        return temp[i].isApproved;
+                }
+            }
+            return !row.row.original.isApproved;
+        }
+        else {
+            if (row) {
+                if (row.emptyRow) {
+                    return false;
+                }
+                else {
+                    for (let i = 0; i < data.length; i++) {
+                        if (row.row.id == data[i].id) {
+                            if (!data[i].isApproved) {
+                                console.log('i ==>  ' + i + " :: " + data[i].id + " -> " + data[i].isApproved);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    const firstUpdate = useRef(true);
+
+    useEffect(() => {
+        if (firstUpdate.current) {
+            firstUpdate.current = false;
+            // console.log("++++++first update+++++++++");
+            return;
+        }
+        console.log("Not the first update");
+        // Do something
     });
 
     const columns = useMemo(
@@ -290,6 +370,8 @@ const AdminHomePage = () => {
             {
                 accessorKey: 'id',
                 header: 'ID',
+                enableColumnOrdering: false,
+                enableEditing: false,
                 size: 50,
             },
             {
@@ -304,25 +386,71 @@ const AdminHomePage = () => {
                 accessorKey: 'lastName',
                 header: 'Last Name',
             },
+            {
+                accessorKey: 'state',
+                header: 'State',
+            },
+            {
+                header: "Artist Name",
+                accessorKey: "isApproved",
+                Cell: ({ cell }) => (
+                    <Tooltip arrow placement="right" title="Approve">
+                        <Switch
+                            checked={handleApproveRenderAndChange(cell, false)}
+                            onChange={() => handleApproveRenderAndChange(cell, true)}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                    </Tooltip>
+                )
+            }
+
         ],
         [],
     );
-
+    const [render, canRender] = useState(true);
+    // canRender(true);
     const [tableData, setTableData] = useState(() => data);
+
     const handleDeleteRow = useCallback(
         (row) => {
-          if (
-            !window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)
-          ) {
-            return;
-          }
-          //send api delete request here, then refetch or update local table data for re-render
-          tableData.splice(row.index, 1);
-          setTableData([...tableData]);
+            if (
+                !window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)
+            ) {
+                return;
+            }
+            //send api delete request here, then refetch or update local table data for re-render
+            tableData.splice(row.index, 1);
+            setTableData([...tableData]);
         },
         [tableData],
-      );
-    
+    );
+
+
+    const [checked, setChecked] = React.useState(true);
+    const handleChange1 = useCallback(
+        (event, row) => {
+            if (window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)) {
+                console.log(event);
+                setChecked(event.target.checked);
+                return;
+            }
+            else {
+                console.log("asdf");
+            }
+            //send api delete request here, then refetch or update local table data for re-render
+            tableData.splice(row.index, 1);
+            setTableData([...tableData]);
+        },
+        [tableData],
+    );
+
+    const handleChange = (event, row) => {
+        console.log(event);
+        console.log(row);
+        setChecked(event.target.checked);
+    };
+
+
     return (
         <div>
             {
@@ -336,31 +464,29 @@ const AdminHomePage = () => {
                                 <h1>youro</h1>
                             </div>
                             <MaterialReactTable
+                                displayColumnDefOptions={{
+                                    'mrt-row-actions': {
+                                        muiTableHeadCellProps: {
+                                            align: 'center',
+                                        },
+                                        size: 120,
+                                    },
+                                }}
                                 columns={columns}
                                 data={data}
                                 enableColumnOrdering
-                                renderDetailPanel={({ row }) => (
-                                    <Box
-                                        sx={{
-                                            display: 'grid',
-                                            margin: 'auto',
-                                            gridTemplateColumns: '1fr 1fr',
-                                            width: '100%',
-                                        }}
-                                    >
-                                        <Typography>Address: {row.original.address}</Typography>
-                                        <Typography>City: {row.original.city}</Typography>
-                                        <Typography>State: {row.original.state}</Typography>
-                                        <Typography>Country: {row.original.country}</Typography>
-                                    </Box>
-                                )}
-                                renderRowActions={({ row, table }) => (
+                                enableRowActions
+                                // enableRowNumbers
+                                positionActionsColumn='last'
+                                renderRowActions={({ row }) => (
                                     <Box sx={{ display: 'flex', gap: '1rem' }}>
-                                        <Tooltip arrow placement="left" title="Edit">
-                                            <IconButton onClick={() => table.setEditingRow(row)}>
-                                                <Edit />
-                                            </IconButton>
-                                        </Tooltip>
+                                        {/* <Tooltip arrow placement="right" title="Approve">
+                                            <Switch
+                                                checked={handleApproveRenderAndChange(row)}
+                                                onChange={(event) => handleChange(event, row)}
+                                                inputProps={{ 'aria-label': 'controlled' }}
+                                            />
+                                        </Tooltip> */}
                                         <Tooltip arrow placement="right" title="Delete">
                                             <IconButton color="error" onClick={() => handleDeleteRow(row)}>
                                                 <Delete />
@@ -368,6 +494,49 @@ const AdminHomePage = () => {
                                         </Tooltip>
                                     </Box>
                                 )}
+                                renderDetailPanel={({ row }) => (
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            margin: 'auto',
+                                            gridTemplateColumns: '1fr 1fr',
+                                            width: '100%',
+                                            padding: '0',
+                                            maxWidth: '100%'
+                                        }}
+                                    >
+                                        <Container maxWidth={false} sx={{ border: '1px solid', padding: '0', margin: '0px', maxWidth: '100%', bgcolor: "#dff7f7" }}>
+                                            <div className='row'>
+                                                <div className='col-12'>
+                                                    <Typography>Address: {row.original.address}</Typography>
+                                                    <Typography>City: {row.original.city}</Typography>
+                                                    <Typography>State: {row.original.state}</Typography>
+                                                    <Typography>Country: {row.original.country}</Typography>
+                                                </div>
+                                                {/* <div className='col-3'>
+                                                    <Tooltip arrow placement="right" title="Delete">
+                                                        <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                                                            <Delete />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </div> */}
+                                            </div>
+                                        </Container>
+                                    </Box>
+                                )}
+                                muiTableBodyProps={{
+                                    sx: () => ({
+                                        // '& tr:nth-of-type(even)': {
+                                        //     backgroundColor: "yellow",
+                                        // },
+                                        '& tr:nth-of-type(odd)': {
+                                            backgroundColor: "lightgray",
+                                            border: '2px solid',
+                                            borderColor: 'black'
+                                        },
+                                    }),
+                                }}
+
                             />
                         </div>
                     </div>
