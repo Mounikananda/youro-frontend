70bcdf6973e26f017f1cd0e673ddf341608aca80
mandavaravi <mandavaravindra1998@gmail.com>
2023-10-12 20:22:05 -0400

    admin patient list integrated

--- youro/src/App.js
+++ youro/src/App.js
@@ -18,10 +18,11 @@ import Home from './views/Home';
 import PatientHomePage from './views/Patient UI/PatientHomePage';
 import DoctorHomePage from './views/Doctor UI/Doctor-HomePage';
 import AdminHomePage from './views/Admin UI/Admin-HomePage';
+import AdminPatientList from './views/Admin UI/Admin-PatientList';
 
 
 export const uTypes = {
-  user : 'USER',
+  user : 'PATIENT',
   doctor : 'PROVIDER',
   admin : 'ADMIN'
 };
@@ -46,7 +47,8 @@ function App() {
         <Route path='/terms-conditions' element={<Terms/>}></Route>
         <Route path='/patient-ui' element={<PatientHomePage/>}></Route>
         <Route path='/doctor-ui' element={<DoctorHomePage/>}></Route>
-        <Route path='/admin-ui' element={<AdminHomePage/>}></Route>
+        <Route path='/admin-doctors' element={<AdminHomePage/>}></Route>
+        <Route path='/admin-patients' element={<AdminPatientList/>}></Route>
      </Routes>
     </Router>
     
