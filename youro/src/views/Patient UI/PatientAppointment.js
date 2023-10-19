bd7479590f267cdc761ffc46f4d553555a625e45
mandavaravi <mandavaravindra1998@gmail.com>
2023-10-18 22:17:43 -0400

    symptom score api integration

--- youro/src/views/Patient UI/PatientAppointment.js
+++ youro/src/views/Patient UI/PatientAppointment.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar as ReactCalendar } from 'react-calendar';
-import { Calendar as BigCalendar, Views, momentLocalizer  } from 'react-big-calendar';
+import { Calendar as BigCalendar, Views, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
@@ -10,61 +10,48 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 const PatientAppointment = (props) => {
     const minDate = new Date();
     const maxDate = new Date();
+
     maxDate.setDate(maxDate.getDate() + 28);
+
     const localizer = momentLocalizer(moment);
+
     const [val, setVal] = useState(-1);
+
     const [event, setEvent] = useState(null);
+
     const [dateSelection, setDateSelection] = useState(new Date());
+    // select data
+
     const [open, setOpen] = useState(false);
+
     const [slotsData, setSlotsData] = useState(['10', '10:30', '11', '11:30', '12', '12:30', '13', '13:30']);
+    // slots in a day
 
     useEffect(() => {
         console.log(event)
     }, [event])
 
-    const myEvents = [{
-        'id': 1,
-        'start': new Date(2023, 9, 14, 10, 0, 0),
-        'end': new Date(2023, 9, 14, 10, 30, 0)
-      },
-      {
-        'id': 2,
-        'start': new Date(2023, 9, 14, 10, 30, 0),
-        'end': new Date(2023, 9, 14, 11, 0, 0)
-      },
-      {
-        'id': 3,
-        'start': new Date(2023, 9, 14, 12, 0, 0),
-        'end': new Date(2023, 9, 14, 12, 30, 0)
-      },
-      {
-        'id': 4,
-        'start': new Date(2023, 9, 14, 13, 0, 0),
-        'end': new Date(2023, 9, 14, 13, 30, 0)
-      },
-    ]
-
-    const handleSelectEvent = (event) => {
-        setVal(event.id)
-        setEvent(event)
-        eventPropGetter(event)
-    }
+    // const handleSelectEvent = (event) => {
+    //     setVal(event.id)
+    //     setEvent(event)
+    //     eventPropGetter(event)
+    // }
 
     const handleBook = () => {
         setOpen(true)
     }
 
-    const eventPropGetter = (event) => {
-        const backgroundColor = event.id === val  ? 'var(--neon-color)' : 'var(--secondary-color)';
-        return { style: { backgroundColor } }
-    }
+    // const eventPropGetter = (event) => {
+    //     const backgroundColor = event.id === val  ? 'var(--neon-color)' : 'var(--secondary-color)';
+    //     return { style: { backgroundColor } }
+    // }
 
     const getEndTime = (time) => {
         var hour = parseInt(time.split(":")[0])
         var minutes = time.split(":")[1]
 
-        if(minutes === '30'){
-            return `${hour+1}`
+        if (minutes === '30') {
+            return `${hour + 1}`
         } else {
             return `${hour}:30`
         }
@@ -78,57 +65,58 @@ const PatientAppointment = (props) => {
     // rnFunc()
 
     return (
-        <div style={{display: 'flex', width: '100%', position: 'relative', alignItems: 'center'}}>
-            <div style={{display: 'flex', width: '70%', justifyContent: 'space-between'}}>
-            <div className="react-calendar-container">
-                <h1 style={{top: '55px', position: 'absolute', left: '50px'}}>Schedule Appointment</h1> 
-                <ReactCalendar minDate={minDate} maxDate={maxDate} onChange={setDateSelection} value={dateSelection}/>
-            </div>
-            <div style={{width: '-webkit-fill-available', marginTop: '10px',}} className="slots-container">
-                <p>Available Slots on - <strong style={{textDecoration: 'underline'}}>{dateSelection.toLocaleDateString()}</strong></p>
-                <div className="slots-container-sub">
-                    {slotsData.map((data) => {
-                        return (
-                            <>
-                            <div onClick={(e) => setEvent(data)} className="slot-timings"><span style={{letterSpacing: '1.3px'}}>{data}</span> - <span style={{letterSpacing: '1.3px'}}>{getEndTime(data)}</span></div>
-                            </>
-                            
-                        )                       
-                    })} 
+        <div style={{ display: 'flex', width: '100%', position: 'relative', alignItems: 'center' }}>
+            <div style={{ display: 'flex', width: '70%', justifyContent: 'space-between' }}>
+                <div className="react-calendar-container">
+                    <h1 style={{ top: '55px', position: 'absolute', left: '50px' }}>Schedule Appointment</h1>
+                    <ReactCalendar minDate={minDate} maxDate={maxDate} onChange={setDateSelection} value={dateSelection} />
+                </div>
+                <div style={{ width: '-webkit-fill-available', marginTop: '10px', }} className="slots-container">
+                    <p>Available Slots on - <strong style={{ textDecoration: 'underline' }}>{dateSelection.toLocaleDateString()}</strong></p>
+                    <div className="slots-container-sub">
+                        {slotsData.map((data) => {
+                            return (
+                                <>
+                                    <div onClick={(e) => setEvent(data)} className="slot-timings"><span style={{ letterSpacing: '1.3px' }}>{data}</span> - <span style={{ letterSpacing: '1.3px' }}>{getEndTime(data)}</span></div>
+                                </>
 
-                    {slotsData && !slotsData[0] && <h4 style={{letterSpacing: '2px'}}>No slots Available on selected date. Choose another date</h4>}
+                            )
+                        })}
+
+                        {slotsData && !slotsData[0] && <h4 style={{ letterSpacing: '2px' }}>No slots Available on selected date. Choose another date</h4>}
+                    </div>
                 </div>
             </div>
-            
-        </div>
-        {event && <>
-                        <div style={{margin: 'auto'}}>
+
+            {
+                event &&
+                <>
+                    <div style={{ margin: 'auto' }}>
                         <strong>Selected Date:</strong>&nbsp;&nbsp;{dateSelection.toDateString()} <br /><br />
                         <strong>Selected Slot:</strong>&nbsp;&nbsp;&nbsp;{event} - {getEndTime(event)}
                         <br /><br /><br /><br /><br />
                         <div className="btn-filled" onClick={handleBook}>Book Appointment</div>
 
-                        </div>
-                        
-                    </>}
+                    </div>
 
-            {/* <div className="congrats-popup"> */}
+                </>
+            }
+
             <Popup open={open} modal closeOnDocumentClick={false} onClose={() => setOpen(false)} className="congrats-popup">
-            <div style={{position: 'absolute', top: '20px', right: '20px', cursor: 'pointer'}} onClick={() => {setOpen(false); props.changeView(0)}}>
-                <span class="material-symbols-outlined">
-                close
-                </span>
-            </div>
-            <div style={{padding: '50px 20px', textAlign: 'center'}}>
-                <h3>Congratulations !!!</h3>
-                <h3>Appointment with Dr. Farah confirmed</h3>
-                <img src={require('../../assets/Congrats.png')} alt='Congrats' style={{height: '100px'}}></img><br/><br/>
-                {event && <p>Appointment at: &nbsp;<strong> {dateSelection.toLocaleDateString()}, {event} - {getEndTime(event)}</strong></p>}
-            </div>
-        </Popup>
-            {/* </div> */}
+                <div style={{ position: 'absolute', top: '20px', right: '20px', cursor: 'pointer' }} onClick={() => { setOpen(false); props.changeView(0) }}>
+                    <span class="material-symbols-outlined">
+                        close
+                    </span>
+                </div>
+                <div style={{ padding: '50px 20px', textAlign: 'center' }}>
+                    <h3>Congratulations !!!</h3>
+                    <h3>Appointment with Dr. Farah confirmed</h3>
+                    <img src={require('../../assets/Congrats.png')} alt='Congrats' style={{ height: '100px' }}></img><br /><br />
+                    {event && <p>Appointment at: &nbsp;<strong> {dateSelection.toLocaleDateString()}, {event} - {getEndTime(event)}</strong></p>}
+                </div>
+            </Popup>
         </div>
-        
+
     )
 }
 
--- youro/src/views/Patient UI/SymptomCalculator.js
+++ youro/src/views/Patient UI/SymptomCalculator.js
@@ -1,85 +1,187 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Popup from 'reactjs-popup';
+import { USER_TYPES } from '../../App';
+import axios from 'axios';
+
 
 const SymptomCalculator = (props) => {
 
-    const diagnosisNames = ['Diagnosis 1', 'Diagnosis 2', 'Diagnosis 3', 'Diagnosis 4', 'Diagnosis 5', 'Diagnosis 6', 'Diagnosis 7', 'Diagnosis 8']
+    const diagnosisNames1 = [{ diagosisId: '1', diagnosisName: 'Diagnosis 1' },
+                            { diagosisId: '2', diagnosisName: 'Diagnosis 2' },
+                            { diagosisId: '3', diagnosisName: 'Diagnosis 3' },
+                            { diagosisId: '4', diagnosisName: 'Diagnosis 4' },
+                            { diagosisId: '5', diagnosisName: 'Diagnosis 5' },
+                            { diagosisId: '6', diagnosisName: 'Diagnosis 6' },
+                            { diagosisId: '7', diagnosisName: 'Diagnosis 7' },
+                            { diagosisId: '8', diagnosisName: 'Diagnosis 8' }];
     const [chooseDiagnosis, setChooseDiagnosis] = useState(true);
-    const [questionnare, setQuestionnare] = useState([{question: 'How are you?', options: ['Good', 'Bad', 'Neutral', 'None'], questionId: '1'},
-                                                     {question: 'What is your name?', options: ['Alan Hunt', 'Farah', 'Both', 'None'],  questionId: '2'}])
+
+    const [questionnare, setQuestionnare] = useState([{ question: 'How are you?', options: ['Good', 'Bad', 'Neutral', 'None'], questionId: '1' },
+    { question: 'What is your name?', options: ['Alan Hunt', 'Farah', 'Both', 'None'], questionId: '2' }])
     const [questionNum, setQuestionNum] = useState(0);
-    const [userResponse, setUserResponse] = useState({})
+
+    const [userResponse, setUserResponse] = useState({});
+
+    const [selDiag, setDiagId] = useState('');
+    const [diagnosisNames, setDiagnoses] = useState([]);
+    const [radioChecked, setChecked] = useState(false);
+
+    useEffect(() => {
+        fetchAllDiagnoses();
+    }, [])
+
+    const fetchQuesByDiagId = async () => {
+        // console.log(selDiag);
+        console.log("====^^^===");
+        console.log("fetchQuesByDiagId START");
+        const url = `http://localhost:9092/youro/api/v1/getQuestionsBydiagId/${selDiag}`;
+        try {
+            const res = await axios.get(url);
+            console.log("res : " + "  =>  " + JSON.stringify(res));
+            setQuestionnare(res.data);
+        }
+        catch (err) {
+            console.error(err);
+        }
+        console.log("fetchQuesByDiagId END");
+        console.log("====^^^===");
+    };
+
+    const fetchAllDiagnoses = async () => {
+        console.log("====^^^===");
+        console.log("fetchAllDiagnoses START");
+        const url = `http://localhost:9092/youro/api/v1/getAllDiagnoses`;
+        try {
+            const res = await axios.get(url);
+            console.log("res : " + "  =>  " + JSON.stringify(res.data));
+            setDiagnoses(res.data);
+        }
+        catch (err) {
+            console.error(err);
+        }
+        console.log("fetchAllDiagnoses END");
+        console.log("====^^^===");
+    };
 
     const handleNext = () => {
-        if(chooseDiagnosis){
-            setChooseDiagnosis(false)
+        console.log("====^^^===");
+        console.log("handleNext START");
+        if (chooseDiagnosis) {
+            console.log("daig true : ");
+            setChooseDiagnosis(false);
+            fetchQuesByDiagId();
 
-            // Get questionnare after user selecting the Diagnosis
-            // axios.post()
         } else {
-            if(questionNum + 1 < questionnare.length) {
+            console.log("daig false : ");
+            console.log("user Res: " + JSON.stringify(userResponse));
+            if (questionNum + 1 < questionnare.length) {
                 setQuestionNum(questionNum + 1)
-            } else{
+            } else {
                 // Post user response data
                 // axios.post()
-                console.log(userResponse)
+                console.log(radioChecked);
+                console.log(userResponse);
+                console.log(selDiag);
+                const now = new Date();
+                console.log(now);
+                const temp = {
+                    dateTime: now,
+                    diagnosisID: selDiag,
+                    questionData: userResponse,
+                    patientId: '1',
+                }
                 props.setOpen(false)
             }
         }
+        console.log("handleNext END");
+        console.log("====^^^===");
     }
 
     const handleResponse = (questionNum, option) => {
-        var userResponses = {...userResponse}
-        userResponses[questionnare[questionNum].questionId] = option;
+        console.log("====^^^===");
+        console.log("handleResponse START");
+        console.log("hand res : " + selDiag);
+        var userResponses = { ...userResponse }
+        userResponses[questionnare[questionNum].questionId] = {option: option, weight: questionnare[questionNum].weight};
+        setChecked(false);
         setUserResponse(userResponses);
+        console.log("handleResponse END");
+        console.log("====^^^===");
+    }
+
+    const handleDiagChange = (event) => {
+        console.log("====^^^===");
+        console.log("handleDiagChange START");
+        console.log("hand diag chng : ");
+        console.log(event.target.value);
+        setDiagId(event.target.value);
+        console.log("handleDiagChange END");
+        console.log("====^^^===");
     }
 
     return (
-            <Popup open={props.open} modal closeOnDocumentClick={false} onClose={() => props.setOpen(false)} className="symptom-popup">
-                <div style={{position: 'absolute', top: '20px', right: '20px', cursor: 'pointer'}} onClick={() => {props.setOpen(false)}}>
-                    <span class="material-symbols-outlined">
+        <Popup
+            open={props.open}
+            modal
+            closeOnDocumentClick={false}
+            onClose={() => props.setOpen(false)}
+            className="symptom-popup">
+
+            <div style={{ position: 'absolute', top: '20px', right: '20px', cursor: 'pointer' }} onClick={() => { props.setOpen(false) }}>
+                <span class="material-symbols-outlined">
                     close
-                    </span>
+                </span>
+            </div>
+            <div style={{ padding: '30px 50px' }}>
+
+                <div style={{ textAlign: 'center' }}>
+                    <h2>Symptom Calculator</h2>
+                    {chooseDiagnosis ? <p>Choose the diagnosis</p> : <p>Question {questionNum + 1} out of {questionnare.length}</p>}
+                    <br />
                 </div>
-                <div style={{padding: '30px 50px'}}>
-                    <div style={{textAlign: 'center'}}>
-                        <h2>Symptom Calculator</h2>
-                        {chooseDiagnosis ? <p>Choose the diagnosis</p> : <p>Question {questionNum+1} out of {questionnare.length}</p>}<br/>
-                    </div>
 
-                    <div style={{textAlign: 'start'}}>
-                        {chooseDiagnosis &&  <div style={{display: "flex", flexWrap: 'wrap'}}>
-                            {diagnosisNames.map((name) => {
-                                return (
-                                    <div style={{width: '200px'}}>
-                                    <input type="radio" id="html" name="diagnosis" value={name}/>
-                                    <label for="html" style={{marginLeft: '10px'}}>{name}</label><br/><br/>
-                                    </div>
-                                )
-                            })}
+                <div style={{ textAlign: 'start' }}>
+                    {chooseDiagnosis && <div style={{ display: "flex", flexWrap: 'wrap' }}>
+                        {diagnosisNames.map((diagosis) => {
+                            return (
+                                <div style={{ width: '200px' }}>
+                                    <input type="radio" id="html" name="diagnosis" value={diagosis.diagId}
+                                        onChange={handleDiagChange} />
+                                    <label for="html" style={{ marginLeft: '10px' }}>{diagosis.name}</label><br /><br />
+                                </div>
+                            )
+                        })}
 
-                        </div> }
-                        {(!chooseDiagnosis) ? <>
-                            <p>{questionnare[questionNum].question}</p>
-                            {questionnare[questionNum].options.map((option) => {
-                                return (
-                                    <>
-                                        <input type="radio" id="html" name={questionnare[questionNum].questionId} checked={userResponse[questionnare[questionNum].questionId] === option} onChange={() => handleResponse(questionNum, option)}/>
-                                        <label for="html" style={{marginLeft: '10px'}}>{option}</label><br/><br/>
-                                    </>
-                                )
-                            })}
+                    </div>}
+                    {(!chooseDiagnosis) ? <>
+                        <p>{questionnare[questionNum].question}</p>
+                        {questionnare[questionNum].options.map((option) => {
+                            return (
+                                <>
+                                    <input type="radio" id="html"
+                                        name={questionnare[questionNum].questionId}
+                                        checked = { userResponse[questionnare[questionNum].questionId] && userResponse[questionnare[questionNum].questionId][option] && (userResponse[questionnare[questionNum].questionId][option] === option) }
+                                        onChange={() => handleResponse(questionNum, option)} />
 
-                        </>: ''}
-                    </div>
+                                    <label for="html" style={{ marginLeft: '10px' }}>{option}</label><br /><br />
+                                </>
+                            )
+                        })}
 
-
-                    <div className={!chooseDiagnosis && !userResponse[questionnare[questionNum].questionId] ? "btn-filled-disabled" : "btn-filled"} style={{width: 'fit-content', marginLeft: 'auto', marginTop: '20px'}} onClick={handleNext}>{chooseDiagnosis || questionNum + 1 < questionnare.length ? 'Next' : 'Submit'}</div>
-                
+                    </> : ''}
                 </div>
-                
-                
-            </Popup>
+
+
+                <div className={!chooseDiagnosis && !userResponse[questionnare[questionNum].questionId] ? "btn-filled-disabled" : "btn-filled"}
+                    style={{ width: 'fit-content', marginLeft: 'auto', marginTop: '20px' }}
+                    onClick={handleNext}>
+                    {chooseDiagnosis || questionNum + 1 < questionnare.length ? 'Next' : 'Submit'}
+                </div>
+
+            </div>
+
+
+        </Popup>
     )
 }
 
