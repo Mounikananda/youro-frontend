0f9b1c49e696e556adfe48422376d07c576f7cfc
jamalpurvamshi12 <vamshivj12@gmail.com>
2023-11-10 02:12:21 -0500

    signup-removed provider+added loader

--- youro/src/views/Signupoptions.js
+++ youro/src/views/Signupoptions.js
@@ -6,30 +6,44 @@ import Loader from "../utils/loader";
 const Signupoptions= () => {
 
     const navigate = useNavigate();
-    const [page, setPage] = useState(0);
+    // const [page, setPage] = useState(1);
     const [activeLoader, setActiveLoader] = useState(false);
 
+    const navigate_to_page=(value)=>
+    {
+      setActiveLoader(true); 
+      setTimeout(() => 
+     {
+      setActiveLoader(false)
+       if (value === 1) {
+        navigate('/signupmyself');
+       } 
+       else {
+      navigate('/signupforfamilymember');
+      }
+     }, 
+     200);
+    }
     return (
         <div>
-        {page === 0 ? <div class="option-select">
+        {/* {page === 0 ? <div class="option-select">
             <h1>youro</h1>
             <h2>Who are you? </h2>
             <p className="h3">Membership gives you or your family access <br/> to a urology-trained provider when you need it.</p>
             <button className="options-button-1" onClick={() => {setPage(1); setActiveLoader(true); setTimeout(() => {setActiveLoader(false)}, 200)}}>Patient</button>
             <button className="options-button-1" onClick={() => navigate('/signupprovider')}>Provider</button>
             </div> 
-            : 
+            :  */}
+            <Loader active={activeLoader}/>
             <div class="option-select">
             <h1>youro</h1>
             <h2>Who is this membership for? </h2>
             <p className="h3">Membership gives you or your family access <br/> to a urology-trained provider when you need it.</p>
-            <button className="options-button-1" onClick={() => navigate('/signupmyself')}>For Myself </button>
-            <button className="options-button-1" onClick={() => navigate('/signupforfamilymember')}>For A Family Member</button>
+            <button className="options-button-1" onClick={() => navigate_to_page(1)}>For Myself </button>
+            <button className="options-button-1" onClick={() => navigate_to_page(2)}>For A Family Member</button>
             </div>
-        }
-
-        <Loader active={activeLoader}/>
        
+     
     </div>
     )
 }
